910042192ae924c7b9eb5782c88fd02d36f94815
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-05-05 22:47:47 +0800

    rubbish

---
+++ src/main/java/_java_model_strategy/Duck.java
@@ -0,0 +1,13 @@
+package _java_model_strategy;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/5/5
+ * @time 22:25
+ */
+public class Duck {
+}
---
+++ src/main/java/_java_model_strategy/Main.java
@@ -0,0 +1,17 @@
+package _java_model_strategy;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/5/5
+ * @time 22:24
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
