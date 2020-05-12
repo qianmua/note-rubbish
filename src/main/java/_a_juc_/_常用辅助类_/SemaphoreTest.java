ea94140d6acd2c8befdba80cd62ef017c6128fe1
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-05-12 21:59:39 +0800

    rubbish

--- src/main/java/_a_juc_/_常用辅助类_/SemaphoreTest.java
+++ src/main/java/_a_juc_/_常用辅助类_/SemaphoreTest.java
@@ -1,7 +1,6 @@
 package _a_juc_._常用辅助类_;
 
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,15 +19,26 @@ public class SemaphoreTest {
          * 一个计数信号量。 在概念上，信号量维持一组许可证。
          * 如果有必要，每个acquire()都会阻塞，直到许可证可用，然后才能使用它。
          * 每个release()添加许可证，潜在地释放阻塞获取方。
+         *
+         * 固定数量同行
+         *
          * */
 
         //限流的时候可以用到
         //并发限流，控制线程池数量
         Semaphore semaphore = new Semaphore(3);
 
+        ThreadPoolExecutor thread = new ThreadPoolExecutor(4,
+                8,
+                3,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(3),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.DiscardOldestPolicy());
+
         for (int i = 0; i < 6; i++) {
             final int finalI = i;
-            new Thread(() ->{
+            thread.execute(() ->{
                 try {
                     // 获得
                     semaphore.acquire();
@@ -44,8 +54,10 @@ public class SemaphoreTest {
                 System.out.println("remove" + finalI);
                 //释放
                 semaphore.release();
-            }).start();
+            });
         }
+
+        thread.shutdown();
 
     }
 }
