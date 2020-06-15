cf81b441ad0a37447526bde0e31076aa3c6019b2
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-06-15 23:35:49 +0800

    rubbish

--- src/main/java/_a_java_model_/design/_3_/struct/Observer.java
+++ src/main/java/_a_java_model_/design/_3_/struct/Observer.java
@@ -1,5 +1,9 @@
 package _a_java_model_.design._3_.struct;
 
+import lombok.*;
+
+import java.util.Observable;
+
 /**
  * @author HJC
  * @version 1.0
@@ -12,8 +16,45 @@ public class Observer {
     * 观察者模式
     * */
     public static void main(String[] args) {
+        Person person = new Person();
+        // 注册观察者
+        person.addObserver(new MyObServer());
 
+        person.setName("1");
+        person.setRealName("2");
     }
 
+
+    static class MyObServer implements java.util.Observer {
+
+        @Override
+        public void update(Observable observable, Object arg) {
+            System.out.println("对象发生变化");
+        }
+    }
+
+    @Getter
+    @ToString
+    @AllArgsConstructor
+    @NoArgsConstructor
+    static class Person extends Observable{
+        private String name;
+        private String realName;
+
+        public void setName(String name) {
+            this.name = name;
+
+            //设置对象发生改变
+            this.setChanged();
+            this.notifyObservers();
+        }
+
+        public void setRealName(String realName) {
+            this.realName = realName;
+
+            this.setChanged();
+            this.notifyObservers();
+        }
+    }
 
 }
