13d57ce8ece71203aa59605ed7845ac8b4af7c47
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-06-16 23:50:42 +0800

    rubbish

--- src/main/java/_a_java_model_/design/_3_/struct/Flyweight.java
+++ src/main/java/_a_java_model_/design/_3_/struct/Flyweight.java
@@ -1,5 +1,7 @@
 package _a_java_model_.design._3_.struct;
 
+import java.util.HashMap;
+
 /**
  * @author HJC
  * @version 1.0
@@ -20,5 +22,55 @@ public class Flyweight {
 
     public static void main(String[] args) {
 
+    }
+}
+
+abstract class Fly{
+    abstract String color();
+
+    public void dusplay(){
+        System.out.println("a" + this.color());
+    }
+}
+
+
+class Factory{
+    private static Factory factory = new Factory();
+
+    private static HashMap<String , Fly> pool;
+
+    private Factory(){
+        pool = new HashMap<>();
+        Fly a , b;
+        a = new A();
+        pool.put("A",a);
+        b = new B();
+        pool.put("B",b);
+    }
+
+
+    public static Factory getFactory(){
+        return factory;
+    }
+
+    public static Fly get(String name){
+        return pool.get(name);
+    }
+}
+
+
+class A extends Fly{
+
+    @Override
+    String color() {
+        return "A";
+    }
+}
+
+class B extends Fly{
+
+    @Override
+    String color() {
+        return "B";
     }
 }
