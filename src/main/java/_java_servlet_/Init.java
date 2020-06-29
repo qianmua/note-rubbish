0425cc67e028dca25a36fd85a0f0ee1761dc0759
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-06-29 21:23:54 +0800

    rubbish

---
+++ src/main/java/_java_servlet_/Init.java
@@ -0,0 +1,23 @@
+package _java_servlet_;
+
+/**
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/6/29  20:35
+ * @description :
+ */
+public class Init {
+
+}
+
+//class A extends C{
+//
+//}
+
+class B {
+
+}
+class C{
+
+}
