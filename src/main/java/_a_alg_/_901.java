afdc4b21ecc295a335073432811b297c0ea8da1b
谦谦君子_卑以自牧也 <2674521520@qq.com>
2020-07-11 15:03:11 +0800

    rubbish vue

--- src/main/java/_a_alg_/_901.java
+++ src/main/java/_a_alg_/_901.java
@@ -28,7 +28,11 @@ public class _901 {
         // you
         dp[0][1] = 0 - prices[0];
         for (int i = 1; i < length; i++) {
+            // 不持有
+            // 不动   卖了
             dp[i][0] = Math.max(dp[i-1][0] , dp[i-1][1] + prices[i]);
+            // 持有
+            // 不懂   前天动了
             dp[i][1] = Math.max(dp[i-1][1] , (i-2 > 0 ? dp[i-2][0] : 0) - prices[i]);
         }
         return Math.max(dp[length -1][0] , dp[length -1][1]);
