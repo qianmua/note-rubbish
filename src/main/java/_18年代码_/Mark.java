be18048d38ef13cc64df7313ec52875800652686
qianmua <49187025+qianmua@users.noreply.github.com>
2020-09-17 23:41:10 +0800

    zookeeper md

---
+++ src/main/java/_18年代码_/Mark.java
@@ -0,0 +1,19 @@
+package _18年代码_;
+
+/**
+ * @author HJC
+ * @version 1.0
+ * 谦谦君子 卑以自牧也
+ * @date 2020/9/17  23:00
+ * @description :
+ */
+public class Mark {
+
+    //TODO 待做
+
+    //FIXME 待修复
+
+    //HACK 粗糙的解决方案
+
+    //XXX 有问题
+}
