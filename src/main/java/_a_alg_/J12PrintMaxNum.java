e17fd8342851f3dc0c70978f3f77fc192799cbf6
qianmua <49187025+qianmua@users.noreply.github.com>
2020-11-23 22:38:36 +0800

    rubbish

--- src/main/java/_a_alg_/J12PrintMaxNum.java
+++ src/main/java/_a_alg_/J12PrintMaxNum.java
@@ -2,6 +2,8 @@ package _a_alg_;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * @author HJC
  * @version 1.0
@@ -17,11 +19,46 @@ public class J12PrintMaxNum {
     public void jm1(){
         //n
         int n = 3; // print 1 2 3 .... 999
+
+
+        int[] arr = {1,234,354,654,75678,6867,867,45,64,534,234,254,};
+
+        quickSort(arr , 0 ,arr.length - 1);
+        System.out.println(Arrays.toString(arr));
     }
 
     private void printMaxNum(int n){
 
         // 注意溢出问题
+
+    }
+
+    // 在交换左右指针的时候只需要先后覆盖就可以了。这样既能减少空间的使用还能降低赋值运算的次数
+    private void quickSort(int[] arr , int v1 , int v2){
+        if (v1 >= v2)
+            return ;
+
+        int l = v1 , r = v2, m  = arr[v1];
+
+        // 分治
+        // 二分
+
+        while (l < r){
+            while (l < r && arr[r] > m)
+                r--;
+
+            arr[l] = arr[r];
+
+            while (l < r && arr[l] <= m)
+                l ++;
+
+            arr[r] = arr[l];
+
+        }
+
+        arr[l] = m;
+        quickSort(arr , v1 , l - 1); // 左
+        quickSort(arr , l + 1 , v2); // 右
 
     }
 }
