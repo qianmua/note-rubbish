286ae7385b9faf2a759ab405012b55aef7c23b4e
qianmua <49187025+qianmua@users.noreply.github.com>
2021-01-06 20:07:25 +0800

    rubbish

---
+++ src/main/java/_a_java_model_/design/_3_/create/singleton/EnumSingleton.java
@@ -0,0 +1,32 @@
+package _a_java_model_.design._3_.create.singleton;
+
+/**
+ * @author jinchao.hu
+ * @version 1.0
+ * @date 2021/1/6  20:01
+ * @description :
+ */
+public class EnumSingleton {
+    private EnumSingleton(){}
+
+
+    enum SingletonEnum{
+        SINGLETON_ENUM;
+
+        private EnumSingleton enumSingleton;
+
+
+        SingletonEnum(){
+            enumSingleton = new EnumSingleton();
+        }
+
+        public static EnumSingleton getInstance() {
+            return SINGLETON_ENUM.enumSingleton;
+        }
+        
+    }
+
+    public static EnumSingleton getInstance() {
+        return SingletonEnum.getInstance();
+    }
+}
